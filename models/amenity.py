25b1055c6e57452efebc68140e596f63dedb90e9
Yousef eslam <yousefeslam214@gmail.com>
2024-04-18 01:25:45 +0200

    tasks

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,11 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
+#!/usr/bin/python3
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    '''The class for Amenity'''
+    __tablename__ = 'amenities'
 
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+    name = Column(String(128), nullable=False)
