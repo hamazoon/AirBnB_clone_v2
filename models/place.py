d385ee37602b33dc8757b608a158a3f3c2583f43
Yousef eslam <yousefeslam214@gmail.com>
2024-04-18 01:13:58 +0200

    8-task

--- models/place.py
+++ models/place.py
@@ -21,3 +21,40 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship(
+            'Review', cascade='all, delete', backref='place')
+        amenities = relationship(
+            'Amenity', secondary=place_amenity, viewonly=False,
+            backref='place_amenities')
+    else:
+        @property
+        def reviews(self):
+            '''FileStorage relationship between Place and Review'''
+            from models import storage
+            from models.review import Review
+
+            review_list = []
+            review_dict = storage.all(Review)
+            for review in review_dict.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            '''
+            Returns the list of `Amenity` instances
+            based on the attribute `amenity_ids` that
+            contains all `Amenity.id` linked to the Place
+            '''
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            '''
+            handles append method for adding an
+            Amenity.id to the attribute amenity_ids
+            '''
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
